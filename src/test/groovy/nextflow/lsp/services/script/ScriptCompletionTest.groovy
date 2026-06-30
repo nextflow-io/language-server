1c2fb863fbe348a0870ce03c801f361c972a88e1
Ben Sherman <bentshermann@gmail.com>
2026-06-30 16:47:32 -0500

    Fix flaky unit test
    Signed-off-by: Ben Sherman <bentshermann@gmail.com>

--- src/test/groovy/nextflow/lsp/services/script/ScriptCompletionTest.groovy
+++ src/test/groovy/nextflow/lsp/services/script/ScriptCompletionTest.groovy
@@ -51,14 +51,14 @@ class ScriptCompletionTest extends Specification {
         def completions = getCompletions(service, uri, new Position(1, 14))
         then:
         completions.size() == 2
-        completions[0].getLabel() == "failOnIgnore"
-        completions[0].getLabelDetails().getDescription() == "Boolean"
-        completions[0].getKind() == CompletionItemKind.Constant
-        completions[0].getDetail() == "failOnIgnore: Boolean"
-        completions[1].getLabel() == "fusion"
-        completions[1].getLabelDetails().getDescription() == "namespace"
-        completions[1].getKind() == CompletionItemKind.Module
-        completions[1].getDetail() == "(namespace) fusion"
+        def failOnIgnore = completions.find { it.getLabel() == "failOnIgnore" }
+        failOnIgnore.getLabelDetails().getDescription() == "Boolean"
+        failOnIgnore.getKind() == CompletionItemKind.Constant
+        failOnIgnore.getDetail() == "failOnIgnore: Boolean"
+        def fusion = completions.find { it.getLabel() == "fusion" }
+        fusion.getLabelDetails().getDescription() == "namespace"
+        fusion.getKind() == CompletionItemKind.Module
+        fusion.getDetail() == "(namespace) fusion"
 
         when:
         open(service, uri, '''\
@@ -69,10 +69,10 @@ class ScriptCompletionTest extends Specification {
         completions = getCompletions(service, uri, new Position(1, 19))
         then:
         completions.size() == 1
-        completions[0].getLabel() == "configFiles"
-        completions[0].getLabelDetails().getDescription() == "List<Path>"
-        completions[0].getKind() == CompletionItemKind.Constant
-        completions[0].getDetail() == "configFiles: List<Path>"
+        def configFiles = completions.find { it.getLabel() == "configFiles" }
+        configFiles.getLabelDetails().getDescription() == "List<Path>"
+        configFiles.getKind() == CompletionItemKind.Constant
+        configFiles.getDetail() == "configFiles: List<Path>"
 
         when:
         open(service, uri, '''\
@@ -83,18 +83,13 @@ class ScriptCompletionTest extends Specification {
         completions = getCompletions(service, uri, new Position(1, 8))
         then:
         completions.size() == 3
-        completions[0].getLabel() == "info"
-        completions[0].getLabelDetails().getDetail() == "(message: String)"
-        completions[0].getKind() == CompletionItemKind.Function
-        completions[0].getDetail() == "info(message: String)"
-        completions[1].getLabel() == "error"
-        completions[1].getLabelDetails().getDetail() == "(message: String)"
-        completions[1].getKind() == CompletionItemKind.Function
-        completions[1].getDetail() == "error(message: String)"
-        completions[2].getLabel() == "warn"
-        completions[2].getLabelDetails().getDetail() == "(message: String)"
-        completions[2].getKind() == CompletionItemKind.Function
-        completions[2].getDetail() == "warn(message: String)"
+        ["info", "error", "warn"].every { name ->
+            def item = completions.find { it.getLabel() == name }
+            item != null \
+                && item.getLabelDetails().getDetail() == "(message: String)" \
+                && item.getKind() == CompletionItemKind.Function \
+                && item.getDetail() == "${name}(message: String)"
+        }
     }
 
     def 'should complete the name of a process in a workflow body' () {
